d967a8dce4ea7d64380593fec11e25f2104fef80
David Fox <dsf@seereason.org>
2008-01-30 19:37:42 +0000

    Add to Eithers: concatEithers, partitionEithers, lefts, rights, isLeft, isRight

--- Extra/Either.hs
+++ Extra/Either.hs
@@ -1,19 +1,34 @@
-module Extra.Either
-    ( rightOnly
-    , eitherFromList
-    ) where
+module Extra.Either where
 
 import Data.Maybe
 
-rightOnly :: [Either a b] -> [b]
-rightOnly = catMaybes . map (either (const Nothing) Just)
+lefts :: [Either a b] -> [a]
+lefts xs = fst (partitionEithers xs)
+
+rights :: [Either a b] -> [b]
+rights xs = snd (partitionEithers xs)
+
+isRight (Right _) = True
+isRight (Left _) = False
+
+isLeft = not . isRight
 
 -- |Turn a list of eithers into an either of lists
-eitherFromList :: [Either a b] -> Either [a] [b]
-eitherFromList l =
-    merge [] [] l
+concatEithers :: [Either a b] -> Either [a] [b]
+concatEithers xs =
+    case partitionEithers xs of 
+      ([], rs) -> Right rs
+      (ls, _) -> Left ls
+
+-- |Return a pair of the lefts and the rights
+partitionEithers :: [Either a b] -> ([a], [b])
+partitionEithers xs =
+    part ([], []) (reverse xs)
     where
-      merge [] r [] = Right r
-      merge l _ [] = Left l
-      merge l r (Left x : etc) = merge (x : l) r etc
-      merge l r (Right x : etc) = merge l (x : r) etc
+      part (ls, rs) [] = (ls, rs)
+      part (ls, rs) (Left l : more) = part (l : ls, rs) more
+      part (ls, rs) (Right r : more) = part (ls, r : rs) more
+
+-- Deprecated
+rightOnly = rights
+eitherFromList = concatEithers
