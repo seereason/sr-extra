789d5c3954c943c852192d566bc7f68f9ec90213
David Fox <dsf@seereason.com>
2008-05-31 23:16:25 +0000

    Version 1.19 - add a sender argument to Extra.Email.sendEmails.

--- Extra/Email.hs
+++ Extra/Email.hs
@@ -12,12 +12,12 @@ import		 Text.MIME.Type hiding (Binary)
 
 
 -- currently, failure is non-fatal, and unreported :-(
-sendEmails :: [(String, String)] -> [(String, [String])] -> IO ()
-sendEmails _ [] = return ()
-sendEmails [] _ = return ()
-sendEmails addrs messages = 
+sendEmails :: (String, String) -> [(String, String)] -> [(String, [String])] -> IO ()
+sendEmails _ _ [] = return ()
+sendEmails _ [] _ = return ()
+sendEmails sender addrs messages = 
     do now <- getZonedTime
-       mapM_ sendEmail (map (uncurry (buildEmail addrs now)) messages)
+       mapM_ sendEmail (map (uncurry (buildEmail sender addrs now)) messages)
 
 sendEmail :: Message String -> IO ()
 sendEmail msg =
@@ -26,10 +26,10 @@ sendEmail msg =
          ExitSuccess -> return ()
          (ExitFailure _n) -> IO.putStrLn outs >> IO.putStrLn errs
 
-buildEmail :: [(String, String)] -> ZonedTime -> String -> [String] -> Message String
-buildEmail addrs origDate subjectText bodyText =
+buildEmail :: (String, String) -> [(String, String)] -> ZonedTime -> String -> [String] -> Message String
+buildEmail sender addrs origDate subjectText bodyText =
     (message
-     (from (addrSpec "autobuilder" "somewhere"))
+     (from (uncurry addrSpec sender))
      origDate
      [ to (map (uncurry addrSpec) addrs), subject $ subjectText]
      (RFC2822Body  bodyText))
