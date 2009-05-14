554114c5b4acbaf4d4298a0e3467e376fa1e515e
Jeremy Shaw <jeremy@seereason.com>
2009-05-14 16:23:21 +0000

    Added Test.QuickCheck.Properties.isIdempotent

---
+++ Test/QuickCheck/Properties.hs
@@ -0,0 +1,9 @@
+module Test.QuickCheck.Properties where
+
+import Test.QuickCheck
+
+isIdempotentBy :: (Arbitrary a, Eq a, Show a) => (a -> a) -> Gen a -> Property
+isIdempotentBy f src = forAll src $ \a -> f a == f (f a)
+
+isIdempotent :: (Arbitrary a, Eq a, Show a) => (a -> a) -> Property
+isIdempotent f = isIdempotentBy f arbitrary
