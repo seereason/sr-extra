99e48c2bdeaedc14848df0f19db6d3680989f01c
David Fox <dsf@seereason.com>
2019-02-21 13:04:28 -0800

    Remove instance Pretty [Loc] it overlaps [a]

--- Extra/TH.hs
+++ Extra/TH.hs
@@ -12,21 +12,19 @@ import Data.List (intersperse)
 #if !MIN_VERSION_base(4,11,0)
 import Data.Monoid ((<>))
 #endif
-import Distribution.Pretty (Pretty(..))
 import Language.Haskell.TH (ExpQ, Loc(..), location)
 import Language.Haskell.TH.Instances ()
 import Language.Haskell.TH.Lift (lift)
---import Text.PrettyPrint (Doc, text)
-import Text.PrettyPrint.HughesPJClass (Doc, hcat, text)
+import Text.PrettyPrint.HughesPJClass (Doc, hcat, Pretty(..), text)
 
 here :: ExpQ
 here = lift =<< location
 
 instance Pretty Loc where
-    pretty = prettyLoc
+    pPrint = prettyLoc
 
 prettyLoc :: Loc -> Doc
 prettyLoc (Loc _filename _package modul (line, col) _) = text (modul <> ":" ++ show line ++ ":" ++ show col)
 
-instance Pretty [Loc] where
-    pretty locs = text "[" <> hcat (intersperse (text " → ") (fmap prettyLoc locs)) <> text "]"
+prettyLocs :: [Loc] -> Doc
+prettyLocs locs = text "[" <> hcat (intersperse (text " ← ") (fmap prettyLoc locs)) <> text "]"
