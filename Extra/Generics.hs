ae5911e6ef7deb3430361e7eec8fcc71d7c5ae87
David Fox <dsf@seereason.com>
2019-05-28 15:48:54 -0700

    Add gshow and gshows functions based on generic-data's gshowsPrec

--- Extra/Generics.hs
+++ Extra/Generics.hs
@@ -1,9 +1,17 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module Extra.Generics
     ( gFind
+    , gshow
+    , gshows
+    , gshowsPrec
     ) where
 
 import Control.Monad (MonadPlus, msum)
-import Data.Generics (Data, listify, Typeable)
+import Data.Generics (Data, listify, Proxy(Proxy), Typeable)
+import Generic.Data (gshowsPrec)
+import Generic.Data.Internal.Show (GShow)
+import GHC.Generics (Generic, Rep)
 
 -- | @gFind a@ will extract any elements of type @b@ from
 -- @a@'s structure in accordance with the MonadPlus
@@ -11,3 +19,10 @@ import Data.Generics (Data, listify, Typeable)
 -- found while [Foo] will return the list of Foos found.
 gFind :: (MonadPlus m, Data a, Typeable b) => a -> m b
 gFind = msum . map return . listify (const True)
+
+-- | Generic version of show based on generic-data's gshowsPrec function.
+gshow :: (Generic a, GShow Proxy (Rep a)) => a -> String
+gshow x = gshows x ""
+
+gshows :: (Generic a, GShow Proxy (Rep a)) => a -> ShowS
+gshows x = gshowsPrec 0 x
