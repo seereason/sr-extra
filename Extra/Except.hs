d5435dcb2ae5da5f9e0fb8e5a3c40f99937a046f
David Fox <dsf@seereason.com>
2019-11-06 10:32:09 -0800

    Version 1.65 - add mtl support for unexceptionalio

--- Extra/Except.hs
+++ Extra/Except.hs
@@ -12,36 +12,27 @@ module Extra.Except
     , handleError
     , HasIOException(fromIOException)
     , IOException'(..)
-#if 1
-    , Tried(unTried)
-    , tryIO
     , logIOError
-#else
-    , LyftIO(lyftIO, IOMonad, ErrorType)
-    , tryLiftIO
-    , logLiftIO
-    , lyftIO'
-    , LyftIO'
-    , LyftIO2
-    , lyftIO2
 
-    , MonadIOError
-    , liftIOError
-    , tryIOError
-    , logIOError
-#endif
+      -- * UnexceptionalIO extensions
+    , HasSomeNonPseudoException(fromSomeNonPseudoException)
+    , lyftIO
+
     , module Control.Monad.Except
     ) where
 
 import Control.Exception ({-evaluate,-} Exception, IOException, SomeException(..))
-import Control.Monad.Catch
 import Control.Monad.Except
---import Control.Monad.Reader (ReaderT)
---import Control.Monad.RWS (RWST)
---import Control.Monad.State (StateT)
---import Control.Monad.Writer (WriterT)
+import Control.Monad.RWS (RWST)
+import Control.Monad.Reader (ReaderT)
+import Control.Monad.State (StateT)
+import Control.Monad.Trans (MonadTrans(lift), liftIO)
+import Control.Monad.Except (ExceptT, runExceptT)
+import Control.Monad.Writer (WriterT)
 import Data.Typeable (typeOf)
 import Extra.Log (logException, Priority(ERROR))
+import UnexceptionalIO (fromIO, SomeNonPseudoException, Unexceptional)
+import qualified UnexceptionalIO as UIO (lift)
 
 -- | Apply a function to whatever @Exception@ type is inside a
 -- @SomeException@:
@@ -110,99 +101,33 @@ newtype IOException' = IOException' IOException
 instance Show IOException' where show (IOException' e) = "(IOException' " <> show (show e) <> ")"
 instance HasIOException IOException' where fromIOException = IOException'
 
-#if 1
-
--- Newtype wrapper around values that are the result of an IO
--- operation invoked with try.  The Tried constructor is private
--- so it can only appear as the result of the lyftIO operation.
-newtype Tried a = Tried {unTried :: a} deriving Functor
-
-tryIO :: (MonadIO m, MonadCatch m, Exception e, MonadError e m) => m a -> ExceptT e m (Tried a)
-tryIO io = lift (try io >>= liftEither . fmap Tried)
-
 logIOError :: (MonadIO m, MonadError e m) => m a -> m a
 logIOError = handleError (\e -> liftIO ($logException ERROR (pure e)) >> throwError e)
 
-#else
+class HasSomeNonPseudoException e where
+  fromSomeNonPseudoException :: SomeNonPseudoException -> e
+instance HasSomeNonPseudoException SomeNonPseudoException where
+  fromSomeNonPseudoException = id
 
-class (MonadIO (IOMonad m),
-       -- The idea of LyftIO is to be a wrapper around a MonadIO
-       -- instance that is not itself a MonadIO instance. that means
-       -- you have to use lyftIO to run IO rather than liftIO, and
-       -- lyftIO always catches exceptions.
-       Exception (ErrorType m),
-       MonadCatch m,
-       MonadCatch (IOMonad m),
-       MonadError (ErrorType m) m,
-       MonadError (ErrorType m) (IOMonad m),
-       -- IOMonad m must have the same error type as m, this is
-       -- generally done through error constraints like
-       -- HasIOException.
-       HasIOException (ErrorType m)) => LyftIO m where
-  type ErrorType m
-  type IOMonad m :: * -> *
-  lyftIO :: IOMonad m a -> m a
+lyftIO :: (MonadError e m, Unexceptional m, HasSomeNonPseudoException e) => IO a -> m a
+--lyftIO io = runExceptT (lyftIO'' io) >>= either throwError return
+--lyftIO io = runExceptT (withExceptT fromSomeNonPseudoException (lyftIO' io)) >>= either throwError return
+--lyftIO io = runExceptT (withExceptT fromSomeNonPseudoException (fromIO io >>= liftEither)) >>= either throwError return
+lyftIO io = fromIO io >>= either (throwError . fromSomeNonPseudoException) return
 
-instance {-# Overlapping #-} (MonadIO m,
-                              e ~ ErrorType m,
-                              Exception e,
-                              MonadCatch m,
-                              MonadError e (IOMonad (ExceptT e m)),
-                              HasIOException e) => LyftIO (ExceptT e m) where
-  type ErrorType (ExceptT e m) = e
-  type IOMonad (ExceptT e m) = m
-  lyftIO io = lift (try io >>= liftEither)
+--lyftIO'' :: (Unexceptional m, HasSomeNonPseudoException e) => IO a -> ExceptT e m a
+--lyftIO'' io = withExceptT fromSomeNonPseudoException (lyftIO' io)
 
-instance LyftIO m => LyftIO (ReaderT r m) where
-  type ErrorType (ReaderT r m) = ErrorType m
-  type IOMonad (ReaderT r m) = IOMonad m
-  lyftIO = lift . lyftIO
-instance LyftIO m => LyftIO (StateT s m) where
-  type ErrorType (StateT s m) = ErrorType m
-  type IOMonad (StateT s m) = IOMonad m
-  lyftIO = lift . lyftIO
-instance (Monoid w, LyftIO m) => LyftIO (WriterT w m) where
-  type ErrorType (WriterT w m) = ErrorType m
-  type IOMonad (WriterT w m) = IOMonad m
-  lyftIO = lift . lyftIO
-instance (Monoid w, LyftIO m) => LyftIO (RWST r w s m) where
-  type ErrorType (RWST r w s m) = ErrorType m
-  type IOMonad (RWST r w s m) = IOMonad m
-  lyftIO = lift . lyftIO
+--lyftIO' :: Unexceptional m => IO a -> ExceptT SomeNonPseudoException m a
+--lyftIO' io = fromIO io >>= liftEither
 
--- | LyftIO analog to the 'try' function.
-tryLiftIO :: LyftIO m => IOMonad m a -> m (Either (ErrorType m) a)
-tryLiftIO = tryError . lyftIO
-
-logLiftIO :: forall m a. LyftIO m => m a -> m a
-logLiftIO = handleError (\e -> lyftIO ($logException ERROR (pure e) :: IOMonad m (ErrorType m)) >> throwError e)
-
-type LyftIO' m = (LyftIO m, IOMonad m ~ ExceptT (ErrorType m) IO)
-
--- | Lift an IO action into any LyftIO instance.  Well, almost any
--- LyftIO instance.
-lyftIO' :: forall m a. (LyftIO' m) => IO a -> m a
-lyftIO' io = lyftIO (withExceptT f (liftIO io))
-  where f :: IOException' -> ErrorType m
-        f = (fromIOException . (\(IOException' e) -> e))
-
-type LyftIO2 e m = (LyftIO' m, e ~ ErrorType m)
-
--- | Lift an @IOMonad m a@ action into @m a@.
-lyftIO2 :: LyftIO2 e m => IOMonad m a -> m a
-lyftIO2 action = lyftIO action
-
--- Backwards compatibiity
-
-type MonadIOError e m = (LyftIO' m, e ~ ErrorType m)
-
-liftIOError :: MonadIOError e m => IO a -> m a
-liftIOError = lyftIO'
-
-tryIOError :: MonadIOError e m => IO a -> m (Either e a)
-tryIOError = tryError . liftIOError
-
-logIOError :: MonadIOError e m => m a -> m a
-logIOError = handleError (\e -> liftIOError ($logException ERROR (pure e)) >> throwError e)
-
-#endif
+instance Unexceptional m => Unexceptional (ExceptT e m) where
+  lift = lift . UIO.lift
+instance Unexceptional m => Unexceptional (ReaderT r m) where
+  lift = lift . UIO.lift
+instance (Unexceptional m, Monoid w) => Unexceptional (RWST r w s m) where
+  lift = lift . UIO.lift
+instance Unexceptional m => Unexceptional (StateT s m) where
+  lift = lift . UIO.lift
+instance (Unexceptional m, Monoid w) => Unexceptional (WriterT w m) where
+  lift = lift . UIO.lift
