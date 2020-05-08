bc140296a1b1341f0615e81c35a7c5b5f887d396
David Fox <dsf@seereason.com>
2020-05-07 22:03:18 -0700

    Remove broken decode function

--- Extra/LocalStorageEncode.hs
+++ Extra/LocalStorageEncode.hs
@@ -1,44 +1,39 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
-module Extra.LocalStorageEncode (encode, decode, decodeM, encodeS, decodeS, urlEncode, urlDecode) where
+module Extra.LocalStorageEncode (encode, decode, encodeS, decodeS, urlEncode, urlDecode) where
 
 import Control.Monad.Except
+import qualified Data.Serialize as Serialize (decode)
 import Extra.Serialize as Serialize (DecodeError(..), fakeTypeRep, Serialize)
-import qualified Extra.Serialize as Serialize (decodeM, encode)
+import qualified Extra.Serialize as Serialize (encode)
 import Data.Text as Text (Text, pack, unpack)
 import qualified Data.Text.Encoding as Text (encodeUtf8, decodeUtf8)
 import Data.Typeable (Proxy(Proxy), Typeable)
 import qualified Data.ByteString.Base64 as Base64 (encode, decode)
-import Extra.Errors (catchMember, runNullExcept, Member, OneOf(..), throwMember)
 import Network.URI (escapeURIString, isUnreserved, unEscapeString)
 
 encode :: Serialize a => a -> Text
 encode = Text.decodeUtf8 . Base64.encode . Serialize.encode
 
--- We can ignore the possibility of Left (OneOf '[])
 decode :: forall a. (Serialize a, Typeable a) => Text -> Either DecodeError a
-decode t = runNullExcept $
-  catchMember
-    (withExceptT @_ @(OneOf '[DecodeError]) @(OneOf '[]))
-    (decodeM t)
-    (\(e :: DecodeError) -> return (Left e))
+#if 0
+decode = either fail return . join . fmap Serialize.decode . Base64.decode . Text.encodeUtf8
+#else
+decode t =
+  case Base64.decode (Text.encodeUtf8 t) of
+    Left s -> throwError (DecodeError (Text.encodeUtf8 t) (fakeTypeRep (Proxy :: Proxy a)) s)
+    Right b ->
+      case Serialize.decode b of
+        Left s -> throwError (DecodeError (Text.encodeUtf8 t) (fakeTypeRep (Proxy :: Proxy a)) s)
+        Right a -> return a
+#endif
 
 encodeS :: Serialize a => a -> String
 encodeS =  Text.unpack . encode
 
 decodeS :: (Serialize a, Typeable a) => String -> Either DecodeError a
 decodeS = decode . Text.pack
-
-decodeM :: forall a e m. (Serialize a, Typeable a, Member DecodeError e, MonadError (OneOf e) m) => Text -> m a
-#if 0
-decodeM = either fail return . join . fmap Serialize.decode . Base64.decode . Text.encodeUtf8
-#else
-decodeM t =
-  case Base64.decode (Text.encodeUtf8 t) of
-    Left s -> throwMember (DecodeError (Text.encodeUtf8 t) (fakeTypeRep (Proxy :: Proxy a)) s)
-    Right b -> Serialize.decodeM b
-#endif
 
 urlEncode :: Serialize a => a -> String
 urlEncode = escape . encodeS
