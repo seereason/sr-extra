9abb1e1193af58abf5382af95a44ea18b78a6f7f
David Fox <dsf@seereason.com>
2020-05-16 05:13:26 -0700

    1.77 - add Extra.QuickCheck and Extra.Rational

---
+++ Extra/QuickCheck.hs
@@ -0,0 +1,42 @@
+module Extra.QuickCheck
+  ( throwResult
+  ) where
+
+import Control.Exception
+import Test.QuickCheck
+
+instance Exception Result
+
+instance Semigroup Result where
+  (Success numTests1 numDiscarded1 labels1 classes1 tables1 output1) <>
+      (Success numTests2 numDiscarded2 labels2 classes2 tables2 output2) =
+    Success (numTests1 + numTests2) (numDiscarded1 + numDiscarded2) (labels1 <> labels2)
+            (classes1 <> classes2) (tables1 <> tables2) (output1 <> "\n" <> output2)
+  (Success _ _ _ _ _ _) <> failure = failure
+  failure <> _ = failure
+
+instance Monoid Result where
+  mempty = Success 0 0 mempty mempty mempty mempty
+  mappend = (<>)
+
+throwResult :: Result -> IO Result
+throwResult result@(Success {}) = return result
+throwResult result = throw result
+
+{-
+Example use:
+
+tests :: IO Result
+tests = do
+  mconcat <$> sequence
+       [quickCheckResult prop_next,
+        quickCheckResult prop_keys,
+        quickCheckResult prop_next,
+        quickCheckResult prop_lookup,
+        quickCheckResult prop_lookupKey,
+        quickCheckResult prop_lookupPair,
+        quickCheckResult prop_splitAt,
+        quickCheckResult prop_uncons,
+        quickCheckResult prop_null,
+        quickCheckResult prop_singleton] >>= throwResult
+-}
---
+++ Extra/Rational.hs
@@ -0,0 +1,68 @@
+module Extra.Rational
+  ( approx
+  -- , rationalIso
+  , rationalLens
+  , rationalIso
+  , readRationalMaybe
+  , showRational
+  ) where
+
+import Control.Lens
+import Data.Ratio ((%), approxRational, denominator, numerator)
+import Numeric ( fromRat, readSigned, readFloat, showSigned, showFFloat )
+import Web.Routes
+
+-- * Rational
+
+-- | Simplify the ratio to avoid a long representation:
+--
+-- > λ> toRational 0.123456
+-- > 8895942329546431 % 72057594037927936
+-- > λ> approxRational (toRational 0.123456) (1 % 10000)
+-- > 10 % 81
+-- > λ> 10 / 81
+-- > 0.12345679012345678   (wow, that's wierd)
+--
+-- This is important for values that might become part of a path,
+-- we don't want them to be too long or subject to rounding errors.
+approx :: Rational -> Rational
+approx x = approxRational x (1 % 10000)
+
+-- | readShowLens is not a good choice for rational numbers,  because
+-- it only understands strings like "15 % 4", not "15" or "3.5".
+-- If an invalid string is input this returns 0.
+rationalLens :: Lens' Rational String
+rationalLens = lens showRational (\r s -> either (const r) id (readRationalMaybe s))
+
+rationalIso :: Iso' Rational String
+rationalIso = iso showRational (readRational 0)
+    where
+      readRational :: Rational -> String -> Rational
+      readRational d = either (const d) id . readRationalMaybe
+
+showRational :: Rational -> String
+showRational x = showSigned (showFFloat Nothing) 0 (fromRat x :: Double) ""
+
+readRationalMaybe :: Monad m => String -> m Rational
+readRationalMaybe s =
+    case (map fst $ filter (null . snd) $ readSigned readFloat s) of
+      [r] -> return r
+      [] -> fail $ "readRationalMaybe " ++ s
+      _rs -> fail $ "readRationalMaybe " ++ s
+
+rsqrt :: Rational -> Rational
+rsqrt = toRational . (sqrt :: Double -> Double) . fromRat
+
+#if 0
+instance View Rational where
+  type ViewType Rational = Text
+  _View = rationalIso . iso pack unpack
+#endif
+
+instance PathInfo Rational where
+  toPathSegments r = toPathSegments (numerator r) <> toPathSegments (denominator r)
+  fromPathSegments = (%) <$> fromPathSegments <*> fromPathSegments
+
+-- mapRatio :: (Integral a, Integral b) => (a -> b) -> Ratio a -> Ratio b
+-- mapRatio f r = f (numerator r) % f (denominator r)
+
