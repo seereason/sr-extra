62bdb33260eb9372f7574a9794d143bf38584c0d
David Fox <dsf@seereason.com>
2020-07-17 13:57:29 -0700

    * Version 1.81 * Add some re-exports from Control.Monad.Except and Extra.Except * Change type variable order of catchMember * Add runOneOf and variants

--- Extra/Errors.hs
+++ Extra/Errors.hs
@@ -38,11 +38,20 @@ module Extra.Errors
   , mapMember
   , runNullExceptT
   , runNullExcept
+  , liftUIO
+  , runOneOf -- I think this is the best one
+  , runOneOf'
+  , runOneOf''
   , test
+  , IOException
+  , module Control.Monad.Except
+  , module Extra.Except
+  , module UnexceptionalIO.Trans
   ) where
 
+import Control.Exception (fromException, IOException, toException)
 import Control.Lens (Prism', prism', review)
-import Control.Monad.Except (Except, ExceptT, MonadError, runExcept, runExceptT, throwError)
+import Control.Monad.Except (Except, ExceptT, MonadError, runExcept, runExceptT, throwError, withExceptT)
 --import Extra.Except (mapError)
 import Data.Type.Bool
 --import Data.Type.Equality
@@ -51,7 +60,8 @@ import Data.SafeCopy
 import qualified Data.Serialize as S (Serialize(get, put), getWord8, Put, PutM, Get)
 import Data.Typeable (Typeable, typeOf)
 import Data.Proxy
-import Extra.Except (tryError)
+import Extra.Except (NonIOException(..), tryError)
+import UnexceptionalIO.Trans (fromIO, SomeNonPseudoException, Unexceptional)
 
 type family IsMember x ys where
   IsMember x '[] = 'False
@@ -138,7 +148,7 @@ instance (IsMember e xs ~ 'True, Get e xs) => Get e (f ': xs) where
   get (Val _e) = Nothing
   get Empty = error "impossible"
 
-oneOf :: ({-IsMember e xs ~ 'True,-} Get e xs, Set e xs) => Prism' (OneOf xs) e
+oneOf :: (Get e es, Set e es) => Prism' (OneOf es) e
 oneOf = prism' set get
 
 type family DeleteList e xs where
@@ -162,10 +172,10 @@ instance {-# OVERLAPS #-} forall e f xs. (Delete e xs, DeleteList e (f:xs) ~ (f 
    delete p (NoVal o) = NoVal (delete p o)
    delete _p Empty = Empty
 
-throwMember :: (Member e es, MonadError (OneOf es) m) => e -> m a
+throwMember :: forall e es m a. (Member e es, MonadError (OneOf es) m) => e -> m a
 throwMember = throwError . review oneOf
 
-liftMember :: (Member e es, MonadError (OneOf es) m) => Either e a -> m a
+liftMember :: forall e es m a. (Member e es, MonadError (OneOf es) m) => Either e a -> m a
 liftMember = either throwMember return
 
 -- | Run an action with @e@ added to the current error set @es@.
@@ -175,16 +185,16 @@ liftMember = either throwMember return
 --   catchMember withExceptT (fileIO @(FileError ': e) (Right <$> query st (LookValue key))) (return . Left)
 -- @@
 catchMember ::
-  forall e es es' m n a.
-  (Member e es, es' ~ DeleteList e es,
-   MonadError (OneOf es) m, MonadError (OneOf es') n)
-  => (forall b. (OneOf es -> OneOf es') -> m b -> n b)
+  forall esplus e es m n a.
+  (Member e esplus, es ~ DeleteList e esplus,
+   MonadError (OneOf esplus) m, MonadError (OneOf es) n)
+  => (forall b. (OneOf esplus -> OneOf es) -> m b -> n b)
   -> m a -> (e -> n a) -> n a
 catchMember helper ma f =
   -- The idea here is that we use tryError to bring a copy of e into
   -- the return value, then we can just delete e from error monad.
   helper (delete @e Proxy) (tryError ma) >>= either handle return
-  where handle :: OneOf es -> n a
+  where handle :: OneOf esplus -> n a
         handle es = maybe (throwError (delete @e Proxy es)) f (get es :: Maybe e)
 
 -- | Simplified catchMember where the monad doesn't change.
@@ -201,6 +211,39 @@ runNullExceptT m = (\(Right a) -> a) <$> runExceptT m
 
 runNullExcept :: Except (OneOf '[]) a -> a
 runNullExcept m = (\(Right a) -> a) (runExcept m)
+
+liftUIO ::
+  (Unexceptional m, Member NonIOException e, Member IOException e, MonadError (OneOf e) m)
+  => IO a
+  -> m a
+liftUIO io =
+  runExceptT (fromIO io) >>= either (either throwMember throwMember . splitException) return
+  where
+    splitException :: SomeNonPseudoException -> Either NonIOException IOException
+    splitException e = maybe (Left (NonIOException e)) Right (fromException (toException e) :: Maybe IOException)
+
+-- | Catch any FileError thrown and put it in the return value.
+runOneOf'' ::
+  forall (esplus :: [*]) e (es :: [*]) m a.
+  (es ~ DeleteList e esplus, Member e esplus, Monad m)
+  => ExceptT (OneOf esplus) m a
+  -> ExceptT (OneOf es) m (Either e a)
+runOneOf'' action = catchMember withExceptT (Right <$> action) (return . Left)
+
+runOneOf' ::
+  forall (esplus :: [*]) e (es :: [*]) m a r.
+  (es ~ DeleteList e esplus, Member e esplus, MonadError (OneOf es) m)
+  => ExceptT (OneOf esplus) m a
+  -> (Either e a -> m r)
+  -> m r
+runOneOf' action final = runExceptT (runOneOf'' action) >>= either throwError final
+
+runOneOf ::
+  forall (esplus :: [*]) e (es :: [*]) m a.
+  (es ~ DeleteList e esplus, Member e esplus, MonadError (OneOf es) m)
+  => ExceptT (OneOf esplus) m a
+  -> m (Either e a)
+runOneOf action = runOneOf' action return
 
 -- ** Example
 
